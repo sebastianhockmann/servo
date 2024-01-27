4925767f6701cbd72a7f059ef0ee4d02aced9bc4
sebastianhockmann <156853550+sebastianhockmann@users.noreply.github.com>
2024-01-27 18:47:49 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
-basic.forever(function () {
-	
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    pins.servoWritePin(AnalogPin.P0, 0)
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    pins.servoWritePin(AnalogPin.P0, 90)
 })
